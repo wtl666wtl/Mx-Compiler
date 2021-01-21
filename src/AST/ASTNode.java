581745338177241542662212461e0769df57aac1
wtl666wtl <2351090351@qq.com>
2021-01-21 14:05:48 +0800

    Add files via upload

--- src/AST/ASTNode.java
+++ src/AST/ASTNode.java
@@ -1,9 +1,11 @@
 package AST;
 
 import Util.position;
+import Util.scope.*;
 
 abstract public class ASTNode {
     public position pos;
+    public Scope scope;
 
     public ASTNode(position pos) {
         this.pos = pos;
