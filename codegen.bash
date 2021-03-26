dea48b731a95f0e5da33d2b264ec826637ede218
wtl666wtl <2351090351@qq.com>
2021-03-26 20:18:07 +0800

    Update codegen.bash

--- codegen.bash
+++ codegen.bash
@@ -1,6 +1,3 @@
-set ff=UNIX
 set -e
 cd "$(dirname "$0")"
-export CCHK="java -classpath ./lib/antlr-4.7.2-complete.jar:./bin: Main -codegen -test"
-cat > test.mx   # save everything in stdin to test.mx
-$CCHK
+cat | java -classpath ./lib/antlr-4.9.1-complete.jar:./bin Main
