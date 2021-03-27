9947fc416e13a10c6102e0b495b206797df346eb
wtl666wtl <2351090351@qq.com>
2021-03-27 11:26:18 +0800

    Add files via upload

---
+++ InstSelector.java
@@ -0,0 +1,358 @@
+package Backend;
+
+import Assembly.AsmInstruction.*;
+import Assembly.AsmInstruction.Ret;
+import Assembly.AsmOperand.*;
+import Assembly.*;
+import Assembly.AsmInstruction.BaseAsmInstruction.*;
+import MIR.*;
+import MIR.IRinstruction.BaseInstruction;
+import MIR.IRinstruction.*;
+import MIR.IRoperand.*;
+import MIR.IRtype.IRBaseType;
+import MIR.IRtype.IRClassType;
+import MIR.IRtype.IRPointerType;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class InstSelector {
+
+    public AsmRootNode AsmRt;
+    public rootNode rt;
+    public AsmBlock curblk;
+    public AsmFunction curFunc;
+    public int vregCounter = 0;
+    public HashMap<Integer, Reg> ImmMap = new HashMap<>();
+    public HashMap<BaseOperand, Reg> regMap = new HashMap<>();
+    public HashMap<Block, AsmBlock> blkMap = new HashMap<>();
+    public HashMap<Function, AsmFunction> funcMap = new HashMap<>();
+
+    public InstSelector(AsmRootNode AsmRt){
+        this.AsmRt = AsmRt;
+    }
+
+    public calType transOpCode(Binary.binaryOpType it){
+        switch (it){
+            case sub: return calType.sub;
+            case add: return calType.add;
+            case mul: return calType.mul;
+            case sdiv: return calType.div;
+            case srem: return calType.rem;
+            case shl: return calType.sll;
+            case ashr: return calType.sra;
+            case and: return calType.and;
+            case or: return calType.or;
+            case xor: return calType.xor;
+            default: return null;
+        }
+    }
+
+    public cmpType transOpCode(Icmp.IcmpOpType it){
+        switch (it){
+            case eq: return cmpType.eq;
+            case ne: return cmpType.ne;
+            case sgt: return cmpType.gt;
+            case sge: return cmpType.ge;
+            case slt: return cmpType.lt;
+            case sle: return cmpType.le;
+            default: return null;
+        }
+    }
+
+    Reg getAsmReg(BaseOperand it){
+        if(it instanceof Register || it instanceof Parameter){
+            if(!regMap.containsKey(it))
+                regMap.put(it, new VirtualReg(vregCounter++, it.type.width / 8));
+            return regMap.get(it);
+        } else if(it instanceof GlobalVar){
+            if(!regMap.containsKey(it)){
+                int pointToWidth = ((IRPointerType)it.type).pointTo.width / 8;
+                String name = ((GlobalVar)it).name;
+                GlobalReg reg = new GlobalReg(name, pointToWidth);
+                regMap.put(it, reg);
+                AsmRt.globalRegs.add(reg);
+                return reg;
+            }
+            return regMap.get(it);
+        } else if(it instanceof ConstString){
+            if(!regMap.containsKey(it)){
+                /*System.out.println(((ConstString) it).name);
+                System.out.println(((ConstString) it).);
+                System.out.println(regMap.containsKey(it));
+                System.out.println(it);*/
+                int pointToWidth = it.type.width / 8;
+                String name = "." + ((ConstString)it).name;
+                GlobalReg reg = new GlobalReg(name, pointToWidth);
+                regMap.put(it, reg);
+                //System.out.println(regMap.containsKey(it));
+                AsmRt.constStrings.put(reg, ((ConstString)it).val);
+                return reg;
+            }
+            return regMap.get(it);
+        } else if(it instanceof ConstInt){
+            int val = ((ConstInt) it).val;
+            if(val == 0)return AsmRt.phyRegs.get(0);
+            if(ImmMap.containsKey(val)) return ImmMap.get(val);
+            VirtualReg reg = new VirtualReg(vregCounter++, 4);
+            curblk.addInst(new Li(reg, curblk, new Imm(val)));
+            ImmMap.put(val, reg);
+            return reg;
+        } else if(it instanceof ConstBool){
+            boolean val = ((ConstBool) it).val;
+            if(!val)return AsmRt.phyRegs.get(0);
+            if(ImmMap.containsKey(1)) return ImmMap.get(1);
+            VirtualReg reg = new VirtualReg(vregCounter++, 4);
+            curblk.addInst(new Li(reg, curblk, new Imm(1)));
+            ImmMap.put(1, reg);
+            return reg;
+        } else return AsmRt.phyRegs.get(0);
+    }
+
+    public void visitRt(rootNode rt){
+        this.rt = rt;
+        rt.builtInFuncs.forEach((funcName, func) ->{
+            AsmFunction AsmFunc = new AsmFunction(funcName, null, null);
+            AsmRt.builtInFuncs.add(AsmFunc);
+            funcMap.put(func, AsmFunc);
+        });
+        rt.funcs.forEach((funcName, func) -> {
+            //optimal? loop
+            func.funcBlocks.forEach(blk -> {
+                AsmBlock Asmblk = new AsmBlock("." + funcName + "_" + blk.name);
+                blkMap.put(blk, Asmblk);
+            });
+            AsmFunction AsmFunc = new AsmFunction(funcName, blkMap.get(func.inblk), blkMap.get(func.outblk));
+            funcMap.put(func, AsmFunc);
+            /*for (int i = 8; i < func.funType.paramList.size(); i++) {
+                Param param = new Param(i, func.funType.paramList.get(i).type.width);
+                regMap.put(func.funType.paramList.get(i), param);
+                AsmFunc.params.add(param);
+            }*/
+            //func.funType.paramList.forEach(parameter -> AsmFunc.params.add(getAsmReg(parameter)));
+            AsmRt.funcs.add(AsmFunc);
+        });
+        rt.funcs.forEach((funcName, func) -> visitFounc(func));
+    }
+
+    void visitFounc(Function func){
+        curFunc = funcMap.get(func);
+        vregCounter = 0;
+        func.funType.paramList.forEach(parameter -> curFunc.params.add(getAsmReg(parameter)));
+        AsmBlock inblk = curFunc.inblk, outblk = curFunc.outblk;
+        ArrayList<VirtualReg> calleeMap = new ArrayList<>();
+        //sp
+        StackLengthImm stackLength = new StackLengthImm(0);
+        stackLength.order = -1;
+        inblk.addInst(new IType(AsmRt.phyRegs.get(2), inblk, AsmRt.phyRegs.get(2), stackLength, calType.add));
+        //if(func.name == "__init")System.out.println(vregCounter);
+        /*AsmRt.calleeRegs.forEach(reg -> {
+            VirtualReg vreg = new VirtualReg(vregCounter++, 4);
+            calleeMap.add(vreg);
+            inblk.addInst(new Mv(vreg, inblk, reg));
+        });*/
+        //ra
+        VirtualReg vreg = new VirtualReg(vregCounter++, 4);
+        inblk.addInst(new Mv(vreg, inblk, AsmRt.phyRegs.get(1)));
+        //if(func.name == "__init")System.out.println(vregCounter);
+        //get params
+        for (int i = 0; i < Integer.min(8, curFunc.params.size()); i++) {
+            inblk.addInst(new Mv(curFunc.params.get(i), inblk, AsmRt.phyRegs.get(10 + i)));
+        }
+        int paramOffest = 0;
+        for (int i = 8; i < curFunc.params.size(); i++) {
+            inblk.addInst(new Ld(curFunc.params.get(i), inblk, AsmRt.phyRegs.get(2),
+                    new StackLengthImm(paramOffest), func.funType.paramList.get(i).type.width / 8));
+            paramOffest = paramOffest + 4;
+        }
+
+        func.funcBlocks.forEach(blk -> {
+            ImmMap.clear();
+            visitBlk(blk);
+            curFunc.blks.add(blkMap.get(blk));
+        });
+
+        /*for (int i = 0; i < AsmRt.calleeRegs.size(); i++) {
+            outblk.addInst(new Mv(AsmRt.calleeRegs.get(i), outblk, calleeMap.get(i)));
+        }*/
+        outblk.addInst(new Mv(AsmRt.phyRegs.get(1), outblk, vreg));
+        outblk.addInst(new IType(AsmRt.phyRegs.get(2), outblk,
+                AsmRt.phyRegs.get(2), new StackLengthImm(0), calType.add));
+        outblk.addInst(new Ret(outblk, AsmRt));
+        curFunc.vregCounter = vregCounter;
+    }
+
+    void visitBlk(Block blk){
+        curblk = blkMap.get(blk);
+        for (BaseInstruction inst : blk.stmts) {
+            if (inst.deleteFlag)continue;
+            //trans to LIR
+            if(inst instanceof Load){
+                Load it = (Load) inst;
+                curblk.addInst(new Ld(getAsmReg(it.rd), curblk,getAsmReg(it.addr),
+                        new Imm(0), it.rd.type.width / 8));
+            } else if(inst instanceof Store){
+                Store it = (Store) inst;
+                Reg addr = getAsmReg(it.addr);
+                if(addr instanceof GlobalReg){
+                    //System.out.println("?");
+                    VirtualReg tmp = new VirtualReg(vregCounter++, 4);
+                    AddrImm addr_high = new AddrImm((GlobalReg) addr, true);
+                    AddrImm addr_low = new AddrImm((GlobalReg) addr, false);
+                    curblk.addInst(new lui(tmp, curblk, addr_high));
+                    curblk.addInst(new St(curblk, tmp, getAsmReg(it.storeVal),
+                                    addr_low, it.storeVal.type.width / 8));
+                } else curblk.addInst(new St(curblk, addr, getAsmReg(it.storeVal),
+                                        new Imm(0), it.storeVal.type.width / 8));
+            } else if(inst instanceof MIR.IRinstruction.Ret){
+                MIR.IRinstruction.Ret it = (MIR.IRinstruction.Ret) inst;
+                if(it.retVal != null){
+                    //put retVal @ a0
+                    Reg reg = getAsmReg(it.retVal);
+                    if(reg instanceof GlobalReg)
+                        curblk.addInst(new La(AsmRt.phyRegs.get(10), curblk, (GlobalReg)reg));
+                    else curblk.addInst(new Mv(AsmRt.phyRegs.get(10), curblk, reg));
+                }
+            } else if(inst instanceof Binary){
+                Binary it = (Binary) inst;
+                //if((it.opCode == Binary.binaryOpType.mul || it.opCode == Binary.binaryOpType.add)){
+                //System.out.print(it.rd.name + " ");
+                //System.out.print((it.lhs) + " ");
+                //System.out.println((it.rhs)); }
+                curblk.addInst(new RType(getAsmReg(it.rd), curblk,
+                        getAsmReg(it.lhs), getAsmReg(it.rhs), transOpCode(it.opCode)));
+            } else if(inst instanceof Icmp){
+                Icmp it = (Icmp) inst;
+                cmpType opCode = transOpCode(it.opCode);
+                //System.out.println(it.arg1);
+                if(opCode == cmpType.lt){
+                    curblk.addInst(new RType(getAsmReg(it.rd), curblk,
+                            getAsmReg(it.arg1), getAsmReg(it.arg2),calType.slt));
+                }else if(opCode == cmpType.gt){
+                    curblk.addInst(new RType(getAsmReg(it.rd), curblk,
+                            getAsmReg(it.arg2), getAsmReg(it.arg1),calType.slt));
+                }else if(opCode == cmpType.le){
+                    VirtualReg tmp = new VirtualReg(vregCounter++, 4);
+                    curblk.addInst(new RType(tmp, curblk, getAsmReg(it.arg2), getAsmReg(it.arg1), calType.slt));
+                    curblk.addInst(new IType(getAsmReg(it.rd), curblk, tmp, new Imm(1), calType.xor));
+                }else if(opCode == cmpType.ge){
+                    VirtualReg tmp = new VirtualReg(vregCounter++, 4);
+                    curblk.addInst(new RType(tmp, curblk, getAsmReg(it.arg1), getAsmReg(it.arg2), calType.slt));
+                    curblk.addInst(new IType(getAsmReg(it.rd), curblk, tmp, new Imm(1), calType.xor));
+                }else{
+                    VirtualReg tmp = new VirtualReg(vregCounter++, 4);
+                    curblk.addInst(new RType(tmp, curblk, getAsmReg(it.arg1), getAsmReg(it.arg2), calType.slt));
+                    VirtualReg tmp2 = new VirtualReg(vregCounter++, 4);
+                    curblk.addInst(new RType(tmp2, curblk, getAsmReg(it.arg2), getAsmReg(it.arg1), calType.slt));
+                    if(opCode == cmpType.ne)
+                        curblk.addInst(new RType(getAsmReg(it.rd), curblk, tmp, tmp2, calType.or));
+                    else{
+                        VirtualReg tmp3 = new VirtualReg(vregCounter++, 4);
+                        curblk.addInst(new RType(tmp3, curblk, tmp, tmp2, calType.or));
+                        curblk.addInst(new IType(getAsmReg(it.rd), curblk, tmp3, new Imm(1), calType.xor));
+                    }
+                }
+            } else if(inst instanceof MIR.IRinstruction.Br){
+                MIR.IRinstruction.Br it = (MIR.IRinstruction.Br)inst;
+                if(it.cond == null){//jp
+                    curblk.addInst(new Jp(curblk, blkMap.get(it.iftrue)));
+                } else {//br
+                    curblk.addInst(new Bz(curblk, getAsmReg(it.cond), cmpType.eq, blkMap.get(it.iffalse)));
+                    curblk.addInst(new Jp(curblk, blkMap.get(it.iftrue)));
+                }
+            } else if(inst instanceof Malloc){
+                Malloc it = (Malloc) inst;
+                curblk.addInst(new Mv(AsmRt.phyRegs.get(10), curblk, getAsmReg(it.length)));
+                curblk.addInst(new Cal(curblk, AsmRt, funcMap.get(rt.builtInFuncs.get("malloc"))));
+                curblk.addInst(new Mv(getAsmReg(it.rd), curblk, AsmRt.phyRegs.get(10)));
+            } else if(inst instanceof Call){
+                Call it = (Call) inst;
+                //System.out.println(it.params.size());
+                for (int j = 0; j < Integer.min(8, it.params.size()); j++) {
+                    Reg reg = getAsmReg(it.params.get(j));
+                    if(reg instanceof GlobalReg)
+                        curblk.addInst(new La(AsmRt.phyRegs.get(10 + j), curblk, (GlobalReg)reg));
+                    else curblk.addInst(new Mv(AsmRt.phyRegs.get(10 + j), curblk, reg));
+                }
+                int paramStSize = 0;
+                for (int j = 8; j < it.params.size(); j++) {
+                    Reg reg = getAsmReg(it.params.get(j));
+                    curblk.addInst(new St(curblk, AsmRt.phyRegs.get(2), reg,
+                            new Imm(paramStSize), it.params.get(j).type.width / 8));
+                    paramStSize = paramStSize + 4;
+                }
+                if(curFunc.paramStSize < paramStSize) curFunc.paramStSize = paramStSize;
+                curblk.addInst(new Cal(curblk, AsmRt, funcMap.get(it.callee)));
+                if(it.rd != null)curblk.addInst(new Mv(getAsmReg(it.rd), curblk, AsmRt.phyRegs.get(10)));
+            } else if(inst instanceof Zext){
+                Zext it = (Zext) inst;
+                Reg reg = getAsmReg(it.orign);
+                if(reg instanceof GlobalReg)curblk.addInst(new La(getAsmReg(it.rd), curblk, (GlobalReg)reg));
+                else curblk.addInst(new Mv(getAsmReg(it.rd), curblk, reg));
+            } else if(inst instanceof GetElementPtr){
+                GetElementPtr it = (GetElementPtr) inst;
+                Reg indexTmp = new VirtualReg(vregCounter++, 4);
+                //indexTmp = target + stepType * stepNum
+                if(it.stepNum instanceof ConstInt){
+                    ConstInt stepNum = (ConstInt) it.stepNum;
+                    if(stepNum.val == 0){
+                        Reg target = getAsmReg(it.target);
+                        if(target instanceof GlobalReg)
+                            curblk.addInst(new La(indexTmp, curblk, (GlobalReg) target));
+                        else curblk.addInst(new Mv(indexTmp, curblk, target));
+                    }else{
+                        curblk.addInst(new RType(indexTmp, curblk, getAsmReg(it.target),
+                                getAsmReg(new ConstInt(stepNum.val * it.stepType.width / 8, 32)), calType.add));
+                    }
+                } else {
+                    Reg tmp = new VirtualReg(vregCounter++, 4);
+                    curblk.addInst(new RType(tmp, curblk, getAsmReg(it.stepNum),
+                            getAsmReg(new ConstInt(it.stepType.width / 8, 32)), calType.mul));
+                    curblk.addInst(new RType(indexTmp, curblk, getAsmReg(it.target), tmp, calType.add));
+                }
+                //index = indexTmp + offset
+                Reg index;
+                if(it.offset == null)index = indexTmp;
+                else{
+                    assert it.offset instanceof ConstInt;
+                    int offset = ((ConstInt)it.offset).val;
+                    if(offset == 0)index = indexTmp;
+                    else{
+                        index = new VirtualReg(vregCounter++, 4);
+                        IRBaseType type = ((IRPointerType)it.target.type).pointTo;
+                        if(type instanceof IRClassType)offset = ((IRClassType)type).getMemberOffset(offset) / 8;
+                        else offset = 0;
+                        curblk.addInst(new RType(index, curblk, indexTmp,
+                                    getAsmReg(new ConstInt(offset, 32)), calType.add));
+                    }
+                }
+
+                if(regMap.containsKey(it.rd))
+                    curblk.addInst(new Mv(regMap.get(it.rd), curblk, index));
+                else regMap.put(it.rd, index);
+            } else if(inst instanceof BitCast){
+                BitCast it = (BitCast) inst;
+                Reg reg = getAsmReg(it.origin);
+                if(reg instanceof GlobalReg)curblk.addInst(new La(getAsmReg(it.rd), curblk, (GlobalReg)reg));
+                else curblk.addInst(new Mv(getAsmReg(it.rd), curblk, reg));
+            } else if(inst instanceof Move){
+                Move it = (Move) inst;
+                if(it.origin instanceof GlobalVar || it.origin instanceof ConstString){
+                    curblk.addInst(new La(getAsmReg(it.rd), curblk, (GlobalReg)getAsmReg(it.origin)));
+                }else if(it.origin instanceof ConstInt){
+                    curblk.addInst(new Li(getAsmReg(it.rd), curblk, new Imm(((ConstInt) it.origin).val)));
+                }else if(it.origin instanceof ConstBool && ((ConstBool) it.origin).val){
+                    curblk.addInst(new Li(getAsmReg(it.rd), curblk, new Imm(1)));
+                }else curblk.addInst(new Mv(getAsmReg(it.rd), curblk,
+                            it.origin instanceof ConstBool ? AsmRt.phyRegs.get(0) :getAsmReg(it.origin)));
+            }
+        }
+        //build AsmBlock's suc/pre
+        blk.sucblks.forEach(sucblk -> {
+            curblk.sucblks.add(blkMap.get(sucblk));
+            //System.out.println(sucblk.name);
+            //System.out.println(blkMap.get(sucblk));
+            blkMap.get(sucblk).preblks.add(curblk);
+        });
+    }
+}
---
+++ Mem2Reg.java
@@ -0,0 +1,166 @@
+package Backend;
+
+import MIR.*;
+import MIR.IRinstruction.BaseInstruction;
+import MIR.IRinstruction.Load;
+import MIR.IRinstruction.Phi;
+import MIR.IRinstruction.Store;
+import MIR.IRoperand.BaseOperand;
+import MIR.IRoperand.Register;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.ListIterator;
+import java.util.Map;
+
+public class Mem2Reg {
+
+    public rootNode rt;
+    HashSet<Block> defblk = new HashSet<>();
+    HashMap<Block, HashSet<Load>> allocLdMap = new HashMap<>();
+    HashMap<Block, HashMap<Register, Phi>> allocPhiMap = new HashMap<>();
+    HashMap<Block, HashMap<Register, BaseOperand>> allocStMap = new HashMap<>();
+
+    HashMap<BaseOperand, BaseOperand> substitutes = new HashMap<>();
+
+    public Mem2Reg(rootNode rt){
+        this.rt = rt;
+    }
+
+    public void work(){
+        rt.funcs.forEach((funcName, func) -> workFunc(func));
+    }
+
+    public void workFunc(Function func){
+        //System.out.println("#########################");
+        //System.out.println(func.name);
+        new DomGen(func).workFunc();//new DomGen(func).workFunc();
+
+        func.funcBlocks.forEach(blk -> {
+            allocLdMap.put(blk, new HashSet<>());
+            allocPhiMap.put(blk, new HashMap<>());
+            allocStMap.put(blk, new HashMap<>());
+        });
+
+        //resolve load/store
+        for(Block blk : func.funcBlocks){
+            //System.out.println("blk name : " + blk.name);
+            for(ListIterator<BaseInstruction> p = blk.stmts.listIterator(); p.hasNext();){
+            //for (BaseInstruction inst : blk.stmts) {
+                //BaseInstruction inst = blk.stmts.get(i);
+                BaseInstruction inst = p.next();
+                if(inst instanceof Load){
+                    BaseOperand addr = ((Load)inst).addr;
+                    if(addr instanceof Register && func.varPtrs.contains(addr)){
+                      //  System.out.println(inst.blk.name);
+                      //  System.out.println(addr);
+                        if(allocStMap.get(inst.blk).containsKey(addr)){
+                            substitutes.put(inst.rd, allocStMap.get(inst.blk).get(addr));
+                            p.remove();
+                            inst.deleteSelf(true);
+                        }else{
+                            //((Load)inst).p = p;
+                            allocLdMap.get(inst.blk).add((Load) inst);
+                        }
+                    }
+                }else if(inst instanceof Store){
+                    BaseOperand addr = ((Store)inst).addr;
+                    if(addr instanceof Register && func.varPtrs.contains(addr)){
+                        //System.out.println(((Store) inst).storeVal);
+                       // System.out.println(((Store) inst).addr);
+                        //System.out.println(inst.blk.name);
+                        defblk.add(inst.blk);
+                        allocStMap.get(inst.blk).put((Register) addr, ((Store) inst).storeVal);
+                        p.remove();
+                        inst.deleteSelf(true);
+                    }
+                }
+            }
+        }
+        //System.out.println("????" + defblk.size());
+        //Phi
+        while(defblk.size() > 0){
+            HashSet<Block> nowblks = defblk;
+            defblk = new HashSet<>();
+            for(Block blk : nowblks){
+                //System.out.println(blk.name);
+                HashMap<Register, BaseOperand> nowAlloc = allocStMap.get(blk);
+                //System.out.println(nowAlloc.size());
+                if(nowAlloc.size() > 0){
+                    for(Block df : blk.domFrontiers){
+                        //System.out.println("Yes!");
+                        for(Map.Entry<Register, BaseOperand> entry : nowAlloc.entrySet()){
+                            Register addr = entry.getKey();
+                            BaseOperand storeVal = entry.getValue();
+
+                            if(!allocPhiMap.get(df).containsKey(addr)){
+                                Register rd = new Register(addr.name + "_phi_Reg", storeVal.type);
+                                Phi phi = new Phi(rd, df, new phiInfo());
+                                //System.out.println("####");
+                                //System.out.println(df.name);
+                                //System.out.println(rd.name);
+                                //System.out.println("####");
+                                df.addPhi(phi);
+                                if(!allocStMap.get(df).containsKey(addr)){
+                                    allocStMap.get(df).put(addr, rd);
+                                    defblk.add(df);
+                                }
+                                allocPhiMap.get(df).put(addr, phi);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        //rename
+        func.funcBlocks.forEach(blk -> {
+            if(!allocPhiMap.get(blk).isEmpty()){
+                allocPhiMap.get(blk).forEach((addr, phi) -> blk.preblks.forEach(preblk -> {
+                    Block nowblk = preblk;
+                    while (!allocStMap.get(preblk).containsKey(addr)) preblk = preblk.iDom;
+                    phi.addOrigin(allocStMap.get(preblk).get(addr), preblk);
+                }));
+            }
+            if(!allocLdMap.get(blk).isEmpty()){
+                allocLdMap.get(blk).forEach(ld -> {
+                    //System.out.println("ld: " + ld.addr);
+                    //System.out.println("ld: " + ld.rd);
+                    //System.out.println("ld: " + ld.blk.name);
+                    Register reg = ld.rd;
+                    Register addr = (Register)ld.addr;
+                    BaseOperand replace;
+                    if(allocPhiMap.get(blk).containsKey(addr))
+                        replace = allocPhiMap.get(blk).get(addr).rd;
+                    else{
+                        Block curblk = blk.iDom;
+                        while(true){
+                            //System.out.println(blk.name);
+                            //System.out.println(blk.iDom);
+                            //if(allocStMap.containsKey(curblk))
+                                if(allocStMap.get(curblk).containsKey(addr)){
+                                replace = allocStMap.get(curblk).get(addr);
+                                break;
+                            } else curblk = curblk.iDom;
+                        }
+                    }
+                    substitutes.put(reg, getReplace(replace));
+                    ld.deleteFlag = true;
+                    ld.deleteSelf(true);
+                });
+            }
+        });
+
+        //substitute All
+        substitutes.forEach((oldOperand, newOperand) ->
+                ((Register)oldOperand).replaceAllUse(getReplace(newOperand)));
+    }
+
+    public BaseOperand getReplace(BaseOperand origin){
+        BaseOperand it = origin;
+        while(substitutes.containsKey(it))
+            it = substitutes.get(it);
+        return it;
+    }
+
+}
