c6cee357ca75f4ba82135cd2c86871b299def3f2
wtl666wtl <2351090351@qq.com>
2021-03-28 09:54:25 +0800

    Add files via upload

--- src/Assembly/AsmInstruction/BaseAsmInstruction.java
+++ src/Assembly/AsmInstruction/BaseAsmInstruction.java
@@ -17,9 +17,11 @@ public abstract class BaseAsmInstruction {
     public Reg rd;
     public AsmBlock blk;
     public BaseAsmInstruction preAdd1 = null, preAdd2 = null;
+    public ArrayList<BaseAsmInstruction> preAdds = new ArrayList<>();
     public BaseAsmInstruction sucAdd1 = null;
     public boolean disableForImm = false;
     public ArrayList<BaseAsmInstruction> instForImm = new ArrayList<>();
+    public ArrayList<BaseAsmInstruction> instForCal = new ArrayList<>();
 
     public BaseAsmInstruction(Reg rd, AsmBlock blk){
         this.rd = rd;
