6096ffccc91b2e158a4774c00945383dbb006ee2
wtl666wtl <2351090351@qq.com>
2021-03-28 11:17:50 +0800

    Add files via upload

--- src/Assembly/AsmOperand/VirtualReg.java
+++ src/Assembly/AsmOperand/VirtualReg.java
@@ -1,8 +1,11 @@
 package Assembly.AsmOperand;
 
+import org.antlr.v4.codegen.model.PlusBlock;
+
 public class VirtualReg extends Reg{
 
     public int index, width;
+    public boolean usedTag = true;
 
     public VirtualReg(int index, int width){
         super();
