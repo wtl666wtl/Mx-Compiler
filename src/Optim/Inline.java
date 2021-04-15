644bf83b5b3b758686c65817ff08ad73e81862bd
wtl666wtl <2351090351@qq.com>
2021-04-15 22:14:21 +0800

    Update Inline.java

--- src/Optim/Inline.java
+++ src/Optim/Inline.java
@@ -16,7 +16,7 @@ public class Inline {
     public boolean flag = false;
     static public int inlineCnt = 0;
     static public int limit = 10;
-    static public int maxLimit = 100;
+    static public int maxLimit = 20;
 
     public Inline(rootNode rt){
         this.rt = rt;
