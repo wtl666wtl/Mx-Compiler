9fc1e37b4eab0667cfd2035acca8db5ce9e70e05
wtl666wtl <2351090351@qq.com>
2021-04-16 21:36:03 +0800

    Add files via upload

--- src/Assembly/AsmBlock.java
+++ src/Assembly/AsmBlock.java
@@ -16,6 +16,7 @@ public class AsmBlock {
     public int notFree = 0;
     public LinkedList<SmartTag> freeList = new LinkedList<>();
     public HashSet<Reg> liveIn =new HashSet<>(), liveOut = new HashSet<>();
+    public int blkCnt = 0;
 
     public AsmBlock(String name, AsmRootNode AsmRt){
         this.name = name;
