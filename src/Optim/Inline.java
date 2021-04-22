c85f8fa2c5972e65cef702b71e72c6f371ac4021
wtl666wtl <2351090351@qq.com>
2021-04-22 23:03:31 +0800

    Update Inline.java

--- src/Optim/Inline.java
+++ src/Optim/Inline.java
@@ -18,7 +18,7 @@ public class Inline {
     static public int inlineCnt = 0;
     static public int addInstCnt = 0;
     static public int addInstLimit = 2147483647;//no limit
-    static public int maxLimit = 100;
+    static public int maxLimit = 90;
 
     public Inline(rootNode rt){
         this.rt = rt;
