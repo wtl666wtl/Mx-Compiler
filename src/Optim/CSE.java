dbe2efe20fb0e32c3e730461cb3405101a80a468
wtl666wtl <2351090351@qq.com>
2021-04-23 20:53:11 +0800

    Add files via upload

--- src/Optim/CSE.java
+++ src/Optim/CSE.java
@@ -76,12 +76,12 @@ public class CSE {
                     else change = true;
                 }
             }
-            curFunc.funcBlocks.forEach(fblk -> {
+            /*curFunc.funcBlocks.forEach(fblk -> {
                 if(fblk.tryDom(blk))workPre(fblk, insts);
-            });
-            /*blk.sucblks.forEach(sucblk -> {
-                if (sucblk.tryDom(blk)) workPre(sucblk, insts);
             });*/
+            blk.sucblks.forEach(sucblk -> {
+                if (sucblk.tryDom(blk)) workPre(sucblk, insts);
+            });
             HashSet<Phi> phis = new HashSet<>();
             for(Iterator<Map.Entry<Register, Phi>> p = blk.Phis.entrySet().iterator(); p.hasNext();){
                 Map.Entry<Register, Phi> entry = p.next();
