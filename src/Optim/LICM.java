fd2b3d1cc14713d739c23f55c9a8b9a05902a915
wtl666wtl <2351090351@qq.com>
2021-04-25 22:56:42 +0800

    Update LICM.java

--- src/Optim/LICM.java
+++ src/Optim/LICM.java
@@ -31,8 +31,9 @@ public class LICM {
     }
 
     public boolean LoadJudge(Loop loop, HashSet<Register> loopDefs, Load ld){
+        if(hasCall)return false;
         if(ld.addr instanceof GlobalVar)
-            return !storeAddr.contains(ld.addr) && !hasCall;
+            return !storeAddr.contains(ld.addr);
         if(!(ld.addr instanceof Register))return false;
         if(ld.addr.type instanceof IRPointerType && ((IRPointerType)ld.addr.type).pointTo instanceof IRPointerType){
             if(loopDefs.contains(ld.addr))return false;
